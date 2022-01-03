477b61f915724a60643cce13512bcfef71e7ceb9
baianinho <renansilva.dev5g@gmail.com>
2022-01-03 16:18:48 -0300

    feat: add validate to input dto

--- src/modules/users/dtos/create-user.input.ts
+++ src/modules/users/dtos/create-user.input.ts
@@ -1,10 +1,19 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsEmail, IsString, Min } from 'class-validator';
 
 @InputType()
 export class CreateUserInput {
   @Field()
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @Min(6)
   username: string;
+
+  @IsString()
+  @Min(8)
   password: string;
+
   avatar?: string;
 }
--- src/modules/users/dtos/update-user.input.ts
+++ src/modules/users/dtos/update-user.input.ts
@@ -1,9 +1,16 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsEmail, IsOptional, Min } from 'class-validator';
 
 @InputType()
 export class UpdateUserInput {
   @Field()
+  @IsEmail()
+  @IsOptional()
   email?: string;
+
+  @Min(6)
+  @IsOptional()
   username?: string;
+
   avatar?: string;
 }
