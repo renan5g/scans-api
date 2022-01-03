3e703f6f56fcc196f171c0dc8c1829ac3aa58254
baianinho <renansilva.dev5g@gmail.com>
2022-01-03 09:00:55 -0300

    feat: prisma module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '@modules/prisma';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [],
   providers: [],
 })
