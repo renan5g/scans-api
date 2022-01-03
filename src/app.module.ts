b79b916175e1a747fc6f8b83a45e1d80fe039312
baianinho <renansilva.dev5g@gmail.com>
2022-01-03 11:04:02 -0300

    feat: graphql config schema file module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+
 import { PrismaModule } from '@modules/prisma';
 import { UsersModule } from '@modules/users';
 
 @Module({
-  imports: [PrismaModule, UsersModule],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    PrismaModule,
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
