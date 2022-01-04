a16148f6d92ca9a500be4f9bf45a02bb2b8ba16a
baianinho <renansilva.dev5g@gmail.com>
2022-01-04 09:48:33 -0300

    feat: config module and auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,24 @@
 import { join } from 'path';
 import { Module } from '@nestjs/common';
+
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 
 import { PrismaModule } from '@modules/prisma';
+import { AuthModule } from '@modules/auth';
 import { UsersModule } from '@modules/users';
 import { GlobalModule } from '@modules/global';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
     PrismaModule,
     UsersModule,
     GlobalModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
