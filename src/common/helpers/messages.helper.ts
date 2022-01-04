7fb6c5c008da1ff1d9e6806e47610285ba130efb
baianinho <renansilva.dev5g@gmail.com>
2022-01-04 17:56:14 -0300

    feat: messages helper

--- src/common/helpers/messages.helper.ts
+++ src/common/helpers/messages.helper.ts
@@ -1,1 +1,13 @@
-export const MessagesHelper = {};
+const PASSWORD_OR_EMAIL_INVALID = 'Email or password invalid';
+const USER_ALREADY_EXISTS = 'User already exists';
+const USER_NOT_FOUND = 'User does not exists';
+const UNAUTHORIZED = 'Unauthorized';
+const CREATE_USER_FAILED = 'Create user failed';
+
+export const MessagesHelper = {
+  PASSWORD_OR_EMAIL_INVALID,
+  USER_ALREADY_EXISTS,
+  USER_NOT_FOUND,
+  UNAUTHORIZED,
+  CREATE_USER_FAILED,
+};
