5a4b4a82cb09a1f1f53c378e5d2c042f46ad9d18
baianinho <renansilva.dev5g@gmail.com>
2022-01-04 09:50:00 -0300

    feat: ad Password and Username Regex

--- src/common/helpers/regex.helper.ts
+++ src/common/helpers/regex.helper.ts
@@ -1,1 +1,6 @@
-export const RexExHelper = {};
+const PASSWORD =
+  /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/;
+
+const USERNAME = /^[a-z0-9_-]{3,15}$/;
+
+export const RegExHelper = { PASSWORD, USERNAME };
