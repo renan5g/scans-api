f0620ed8a85dcfbd8f45b84254bf4741e5951a8e
baianinho <renansilva.dev5g@gmail.com>
2022-01-05 09:50:16 -0300

    bugfix: remove base repository prisma

--- src/modules/users/repositories/users.repository.ts
+++ src/modules/users/repositories/users.repository.ts
@@ -1,34 +1,37 @@
 import { Injectable } from '@nestjs/common';
 
-import { BaseRepositoryPrisma } from '@common/classes';
+import { PrismaService, Prisma } from '@modules/prisma';
 import { CreateUserInput, UpdateUserInput } from '../dtos';
 import { User } from '../models';
 
 @Injectable()
-export class UsersRepository extends BaseRepositoryPrisma<User, 'user'> {
-  constructor() {
-    super(User, 'user');
-  }
+export class UsersRepository {
+  constructor(private readonly prismaService: PrismaService) {}
 
   async existsByEmail(email: string): Promise<boolean> {
-    return this.exists({ email });
+    return await this.exists({ email });
+  }
+
+  async existsById(id: string): Promise<boolean> {
+    return await this.exists({ id });
   }
 
   async getAll(): Promise<User[] | null> {
     const users = await this.prismaService.user.findMany();
-    return this.formatMany(users);
+    return users;
   }
 
   async getByEmail(email: string): Promise<User | null> {
-    const user = await this.prismaService.user.findUnique({
-      where: { email },
-    });
-    return this.format(user);
+    return await this.get({ email });
+  }
+
+  async getById(id: string): Promise<User | null> {
+    return await this.get({ id });
   }
 
   async create(input: CreateUserInput): Promise<User> {
     const user = await this.prismaService.user.create({ data: input });
-    return this.format(user);
+    return user;
   }
 
   async update(id: string, input: UpdateUserInput): Promise<User> {
@@ -36,7 +39,7 @@ export class UsersRepository extends BaseRepositoryPrisma<User, 'user'> {
       where: { id },
       data: input,
     });
-    return this.format(user);
+    return user;
   }
 
   async delete(id: string): Promise<boolean> {
@@ -44,5 +47,19 @@ export class UsersRepository extends BaseRepositoryPrisma<User, 'user'> {
       where: { id },
     });
     return !!deleted;
+  }
+
+  private async exists(where?: Prisma.UserWhereUniqueInput): Promise<boolean> {
+    const count = await this.prismaService.user.count({
+      where,
+    });
+    return count > 0;
+  }
+
+  private async get(where?: Prisma.UserWhereUniqueInput): Promise<User> {
+    const result = await this.prismaService.user.findUnique({
+      where,
+    });
+    return result;
   }
 }
