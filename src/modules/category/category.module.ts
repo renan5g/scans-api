69c585f201de7f96710cf0a8b126740117406bcb
baianinho <renansilva.dev5g@gmail.com>
2022-01-06 08:44:32 -0300

    feat: create category service and resolver

--- src/modules/category/category.module.ts
+++ src/modules/category/category.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { CategoryService } from '@modules/category/services';
+import { CategoryResolver } from '@modules/category/resolvers';
 
-@Module({})
+@Module({
+  providers: [CategoryService, CategoryResolver],
+})
 export class CategoryModule {}
